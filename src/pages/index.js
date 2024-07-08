d304f6528b40dca4d0d9c26c1245eb409e3b7f4c
Alex Burduja <burduja_alex@yahoo.com>
2024-07-08 17:01:17 +0300

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -555,7 +555,7 @@ export default function Home() {
       <section className='contact' id='contact'>
         <div className='contactHeader'>
           <h1>Contact me!</h1>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467.6671151670515!2d26.14054986334789!3d44.415803951511364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1feeb9bafa12d%3A0x7df67b912179f52c!2zU3RyYWRhIFLDom1uaWN1IFbDomxjZWEgMTksIEJ1Y3VyZciZdGk!5e0!3m2!1sen!2sro!4v1697654067560!5m2!1sen!2sro" width="100%" height="450" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467.6671151670515!2d26.14054986334789!3d44.415803951511364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1feeb9bafa12d%3A0x7df67b912179f52c!2zU3RyYWRhIFLDom1uaWN1IFbDomxjZWEgMTksIEJ1Y3VyZciZdGk!5e0!3m2!1sen!2sro!4v1697654067560!5m2!1sen!2sro" width="100%" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
         <div className='inTouchFlex'>
