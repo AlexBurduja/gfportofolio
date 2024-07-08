ae49f3da2e7bf052059fd0633094a10556a4f5a7
Alex Burduja <burduja_alex@yahoo.com>
2024-07-08 16:54:57 +0300

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,12 +15,13 @@ import Slider from 'react-slick'
 import dynamic from 'next/dynamic';
 
 const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
-import video1 from '../../public/video1.mov'
+
 import video2 from '../../public/video2.mov'
 import video3 from '../../public/video3.mov'
 import video4 from '../../public/video4.mov'
 import video5 from '../../public/video5.mov'
 import video6 from '../../public/video6.mov'
+import video6 from '../../public/video7.mov'
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
